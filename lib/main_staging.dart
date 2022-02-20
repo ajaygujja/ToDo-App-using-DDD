428a5e75533cae7bc96a0616b861d0a3092ba6f8
Ajay <ajay.gujja@siply.in>
2022-02-20 15:58:31 +0530

    Removed Stagging and Readme changed

--- lib/main_staging.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_ddd/bootstrap.dart';
-import 'package:flutter_ddd/injection.dart';
-import 'package:flutter_ddd/presentation/core/app_widget.dart';
-import 'package:injectable/injectable.dart';
-
-void main() {
-  configureInjection(Environment.test);
-  bootstrap(() => AppWidget());
-}
