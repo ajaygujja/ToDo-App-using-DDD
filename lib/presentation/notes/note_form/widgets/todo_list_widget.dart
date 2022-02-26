2e1513e3a741b74eb9e10a0d45462d1d4dff554b
ajaygujja <ajaygujja5@gmail.com>
2022-02-26 15:53:19 +0530

    Todo List Widget

--- lib/presentation/notes/note_form/widgets/todo_list_widget.dart
+++ lib/presentation/notes/note_form/widgets/todo_list_widget.dart
@@ -2,6 +2,11 @@ import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ddd/application/notes/note_form/note_form_bloc.dart';
+import 'package:flutter_ddd/presentation/notes/note_form/misc/build_context_x.dart';
+import 'package:flutter_ddd/presentation/notes/note_form/misc/todo_item_presentation_classes.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:kt_dart/collection.dart';
+import 'package:provider/provider.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({Key? key}) : super(key: key);
@@ -9,11 +14,13 @@ class TodoList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocListener<NoteFormBloc, NoteFormState>(
+      listenWhen: (previous, current) =>
+          previous.note.todos.isFull != current.note.todos.isFull,
       listener: (context, state) {
         if (state.note.todos.isFull) {
           FlushbarHelper.createAction(
             message: 'Want longer lists? Activate Premium',
-            button: ElevatedButton(
+            button: TextButton(
               onPressed: () {},
               child: const Text(
                 'BUY NOW',
@@ -24,7 +31,43 @@ class TodoList extends StatelessWidget {
           ).show(context);
         }
       },
-      child: Container(),
+      child: Consumer<FormTodos>(
+        builder: (context, formTodos, child) {
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: formTodos.value.size,
+            itemBuilder: (context, index) {
+              return TodoTile(
+                index: index,
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class TodoTile extends HookWidget {
+  final int index;
+  const TodoTile({required this.index, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final todo =
+        context.formTodos.getOrElse(index, (_) => TodoItemPrimitive.empty());
+    return ListTile(
+      leading: Checkbox(
+        onChanged: (value) {
+          context.formTodos = context.formTodos.map((listTodo) =>
+              listTodo == todo ? todo.copyWith(done: value!) : listTodo);
+
+          context
+              .read<NoteFormBloc>()
+              .add(NoteFormEvent.todosChanged(context.formTodos));
+        },
+        value: todo.done,
+      ),
     );
   }
 }
